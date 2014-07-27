8ef228c9178c96e0560fb01a3b04f1fdfbbdd573
Agustin Muniz <gus.s.muniz@gmail.com>
2014-07-26 23:24:42 -0700

    added date range for chart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,10 @@
 $( "#weigh_in_day" ).datepicker({
 	dateFormat: 'yy-mm-dd'
 });
+$( "#start_date" ).datepicker({
+	dateFormat: 'yy-mm-dd'
+});
+$( "#end_date" ).datepicker({
+	dateFormat: 'yy-mm-dd'
+});
 });
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,19 @@ class StaticPagesController < ApplicationController
       @weigh_in = current_user.weigh_ins.build
       @feed_items = current_user.feed.paginate(page: params[:page])
       @current_user_id = current_user.id
+      sDate = params[:start_date]
+      if sDate.nil?
+        @start_date = 3.weeks.ago.to_date
+      else
+        @start_date = sDate
+      end
+      
+      eDate = params[:end_date]
+      if eDate.nil?
+        @end_date = Date.today
+      else
+        @end_date = eDate
+      end
     end
   end
 
